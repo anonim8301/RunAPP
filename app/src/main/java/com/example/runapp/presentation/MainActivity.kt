7ef337e9f92e1fa0c796027bdf3d82bfe0db97e4
Ace Card <anonim8301@gmail.com>
2022-05-24 14:58:06 +0300

    starting

---
+++ app/src/main/java/com/example/runapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.runapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.runapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
